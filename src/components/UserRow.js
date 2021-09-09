fd4a6051db402554c833ded5721da564e1d809d9
yoonhero <yoonhero06@naver.com>
2021-09-10 08:23:33 +0900

    Functions - UnFollow

--- src/components/UserRow.js
+++ src/components/UserRow.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { realtimeDatabase } from "../fbase";
 
 const UserRow = ({ uid, avatar, username, userObj }) => {
+  const [follows, setFollows] = useState(false);
+
+  const getButton = async () => {
+    await realtimeDatabase
+      .ref(`users/${userObj.uid}/follow`)
+      .once("value", function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+          console.log(childSnapshot.val());
+          if (childSnapshot.val() == uid) {
+            setFollows(true);
+            return;
+          }
+        });
+      });
+  };
+
+  useEffect(async () => {
+    await getButton();
+  }, []);
+
   const follow = async () => {
     let alreadyFollow = [];
     await realtimeDatabase
@@ -11,19 +31,25 @@ const UserRow = ({ uid, avatar, username, userObj }) => {
           alreadyFollow.push(childSnapshot.val());
         });
       });
+
     let newFollow = [];
     let isDup = false;
-    alreadyFollow.map((auid) => {
+    alreadyFollow.map((auid, i) => {
       if (auid == uid) {
         isDup = true;
+        alreadyFollow.splice(i, 1);
         return;
       }
     });
     if (!isDup) {
       newFollow = [...alreadyFollow, uid];
+    } else {
+      newFollow = [...alreadyFollow];
     }
 
-    await realtimeDatabase.ref(`users/${userObj.uid}/follow`).update(newFollow);
+    await realtimeDatabase.ref(`users/${userObj.uid}/follow`).set(newFollow);
+    setFollows(false);
+    await getButton();
   };
 
   return (
@@ -37,7 +63,9 @@ const UserRow = ({ uid, avatar, username, userObj }) => {
         }
       />
       <div>{username}</div>
-      <button onClick={() => follow()}>follow</button>
+      <button onClick={() => follow()}>
+        {follows ? "UnFollow" : "Follow"}
+      </button>
     </div>
   );
 };
