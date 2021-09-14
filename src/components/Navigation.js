fa85a38886d758f99fc5697559a4293bcf877545
yoonhero <yoonhero06@naver.com>
2021-09-15 08:15:38 +0900

    FrontEnd - Bottom Nav Bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,42 +2,30 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faSearch, faUser } from "@fortawesome/free-solid-svg-icons";
+import "./Navigation.css";
 
 const Navigation = ({ userObj }) => (
-  <nav>
-    <ul
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        marginTop: 50,
-      }}>
+  <nav class='bottom-nav'>
+    <ul class='nav-items'>
       <li>
-        <Link to='/' style={{ marginRight: 10 }}>
-          <FontAwesomeIcon icon={faTwitter} color={"#04AAFF"} size='2x' />
+        <Link to='/' class=''>
+          <FontAwesomeIcon icon={faHome} size='lg' />
+          {/* <span>Home</span> */}
         </Link>
       </li>
       <li>
-        <Link
-          to='/profile'
-          style={{
-            marginLeft: 10,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            fontSize: 12,
-          }}>
-          <FontAwesomeIcon icon={faUser} color={"#04AAFF"} size='2x' />
-          <span style={{ marginTop: 10 }}>
-            {userObj.displayName
-              ? `${userObj.displayName}의 Profile`
-              : "Profile"}
-          </span>
+        <Link to='/profile' class=''>
+          <FontAwesomeIcon icon={faUser} size='lg' />
+          {/* <span>
+            {userObj.displayName ? `${userObj.displayName}` : "Profile"}
+          </span> */}
         </Link>
       </li>
       <li>
-        <Link to='/search'>
-          <span>Search</span>
+        <Link to='/search' class=''>
+          <FontAwesomeIcon icon={faSearch} size='lg' />
+          {/* <span>Search</span> */}
         </Link>
       </li>
     </ul>
